b5a43c813a3733d07bddf94aafd117dcf5da7b25
haniasamad <haniasamad04@gmail.com>
2024-01-09 07:01:02 -0800

    some-code

---
+++ script.js
@@ -0,0 +1,24 @@
+var foods = ["Pizza", "Burger", "Ice Cream"]
+// var foodSays = prompt("Ënter the Food you would like to have")
+
+// if (foodSays === foods[0]) {
+//     alert("You are Right")
+// }
+// else if (foodSays === foods[1]) {
+//     alert("You are Right")
+// }
+// else if (foodSays === foods[2]) {
+//     alert("You are Right")
+// }
+// else {
+//     alert("You are wrong!")
+// }
+
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log("Hello World");
+// }
+
+for (var i = 0; i < foods.length; i++) {
+    console.log(foods[i]);
+}
